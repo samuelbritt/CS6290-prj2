c916233ca74263ad744f597c7558eb4d400703a1
Sam Britt <samuelbritt@gmail.com>
2012-03-08 23:42:51 -0500

    getopt

--- tomasulo_sim.c
+++ tomasulo_sim.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <getopt.h>
 
 /* Max number of source registers for any instruction */
 #define SRC_REGISTER_COUNT 2
@@ -14,6 +16,9 @@
 #define FU0_COUNT 1
 #define FU1_COUNT 1
 #define FU2_COUNT 1
+
+/* Print information at each step */
+static int verbose;
 
 struct int_register {
 	bool ready;
@@ -55,7 +60,65 @@ struct func_unit {
 	int latency;
 };
 
-int main(int argc, char const *argv[])
+static void process_options(int argc, char *const argv[])
+{
+	int c;
+	int k0 = 0;
+	int k1 = 1;
+	int k2 = 2;
+	while (1) {
+		static struct option opt[] = {
+			/* {name, has_arg, flag, val} */
+			{"k0", required_argument, NULL, '0'},
+			{"k1", required_argument, NULL, '1'},
+			{"k2", required_argument, NULL, '2'},
+			{"verbose", no_argument, &verbose, 'v'},
+			{0, 0, 0, 0}
+		};
+
+		int option_index = 0;
+		c = getopt_long(argc, argv, "0:1:2:v", opt, &option_index);
+
+		if (c == -1)
+			break;
+
+		switch (c) {
+			case '0':
+				k0 = atoi(optarg);
+				break;
+			case '1':
+				k1 = atoi(optarg);
+				break;
+			case '2':
+				k2 = atoi(optarg);
+				break;
+			case 'v':
+				verbose = true;
+				break;
+			case 0:
+				// flags
+				if (verbose == 'v')
+					verbose = true;
+				break;
+			case '?':
+				break;
+			default:
+				abort();
+		}
+	}
+
+	if (verbose)
+		printf("Running in verbose mode\n");
+	printf("ks: %d, %d, %d\n", k0, k1, k2);
+
+	printf("args: ");
+	while (optind < argc) {
+		printf("%s ", argv[optind++]);
+	}
+	printf("\n");
+}
+
+int main(int argc, char * const argv[])
 {
 	struct reservation_station *sched_queue;
 	struct int_register reg_file[REGISTER_COUNT];
@@ -66,6 +129,8 @@ int main(int argc, char const *argv[])
 	struct func_unit fu1[FU1_COUNT];
 	struct func_unit fu2[FU2_COUNT];
 	struct func_unit *fus[] = {fu0, fu1, fu2};
+
+	process_options(argc, argv);
 
 	printf("Hello world\n");
 	return 0;
