1922b761345eade362fef22629ac863d175fab95
Sam Britt <samuelbritt@gmail.com>
2012-03-11 01:06:26 -0500

    comments, formatting

--- deque.h
+++ deque.h
@@ -11,24 +11,54 @@
 typedef struct deque deque_t;
 typedef struct deque_node deque_node_t;
 
+/*
+ * Creation functions
+ */
 deque_t *deque_create();
-void deque_destroy(deque_t *deque);
 
 deque_node_t *deque_create_node(void *data);
-deque_node_t *deque_delete_node(deque_t *deque, deque_node_t *node);
-void deque_destroy_node(deque_node_t *node);
 
+
+/*
+ * Update functions
+ */
 deque_node_t *deque_insert_before(deque_t *deque, deque_node_t *node,
 				  deque_node_t *new_node);
+
 deque_node_t *deque_insert_after(deque_t *deque, deque_node_t *node,
 				 deque_node_t *new_node);
+
+/* add to end of deque */
 deque_node_t *deque_append(deque_t *deque, deque_node_t *node);
+
+/* add to start of deque */
 deque_node_t *deque_prepend(deque_t *deque, deque_node_t *node);
 
-/* Functions to allow walking through deque */
+
+/*
+ * Reading functions
+ */
 bool deque_is_empty(deque_t *deque);
+
+/* start(), next(), and end() allow walking through deque */
+/* returns the first node in the deque */
 deque_node_t *deque_start(deque_t *deque);
+
+/* returns the node after the given node */
 deque_node_t *deque_next(deque_t *deque, deque_node_t *node);
-bool deque_end(deque_t *deque, deque_node_t *node); /* returns true if last node */
+
+/* returns true if `node` is the last in the deque */
+bool deque_end(deque_t *deque, deque_node_t *node);
+
+
+/*
+ * Delete functions
+ */
+void deque_destroy(deque_t *deque);
+
+/* Deletes from deque, but does not destroy the node */
+deque_node_t *deque_delete_node(deque_t *deque, deque_node_t *node);
+
+void deque_destroy_node(deque_node_t *node);
 
 #endif /* end of include guard: DEQUE_H_ */
