16386336ac94266794509dd0aa3365c0086a4874
Sam Britt <samuelbritt@gmail.com>
2012-04-03 19:49:17 -0400

    clean up cruft from state update

--- src/state_update.c
+++ src/state_update.c
@@ -6,44 +6,31 @@
 
 #include "deque.h"
 
-static struct {
-	deque_t *queue;
-	int cdb_total_count;
-	int cdb_use_count;
-} su_state;
-
-static int
-update_sched_queue(void *completed_rs_)
+static void
+update_sched_queue(struct reservation_station *completed_rs)
 {
-	struct reservation_station *completed_rs = completed_rs_;
-
 	struct cdb cdb;
 	cdb.tag = completed_rs->dest.tag;
 	cdb.reg_num = completed_rs->dest.index;
 	cdb.val = 0;
 	sched_broadcast_cdb(&cdb);
 	sched_delete_rs(completed_rs);
-	return DEQUE_CONTINUE;
 }
 
-static int
-update_reg_file(void *rs_, void *reg_file_)
+static void
+update_reg_file(struct reservation_station *rs, struct int_register *reg_file)
 {
-	struct reservation_station *rs = rs_;
-	struct int_register *reg_file = reg_file_;
-
 	if (rs->dest.index >= 0) {
 		struct int_register *reg = &reg_file[rs->dest.index];
 		if (rs->dest.tag == reg->tag)
 			reg->ready = true;
 	}
-	return DEQUE_CONTINUE;
 }
 
 void
-state_update(struct int_register *reg_file)
+state_update(int cdb_count, struct int_register *reg_file)
 {
-	for (int i = 0; i < su_state.cdb_total_count; ++i) {
+	for (int i = 0; i < cdb_count; ++i) {
 		struct reservation_station *rs = exe_retire_inst();
 		if (!rs)
 			return;
@@ -51,16 +38,4 @@ state_update(struct int_register *reg_file)
 		update_reg_file(rs, reg_file);
 		update_sched_queue(rs);
 	}
-}
-
-void su_destroy()
-{
-	deque_destroy(su_state.queue);
-}
-
-void su_init(int cdb_count)
-{
-	su_state.queue = deque_create();
-	su_state.cdb_total_count = cdb_count;
-	su_state.cdb_use_count = 0;
 }
--- src/state_update.h
+++ src/state_update.h
@@ -7,19 +7,8 @@
 
 #include "common.h"
 
-/* Call before anything else */
-void su_init(int cdb_count);
-
-/* Attempts to retire the instruction and return 0. If it can't (because all the
- * CDBs are busy), returns 1 */
-int su_retire_inst(struct reservation_station *rs);
-
 /* Updates both the scheduling queue and the register file where necessary.
  * Updates are done in tag order from the CDBs */
-void state_update(struct int_register *reg_file);
-
-/* frees any memory used by the state update unit */
-void su_destroy();
+void state_update(int cdb_count, struct int_register *reg_file);
 
 #endif /* end of include guard: STATE_UPDATE_H_ */
-
