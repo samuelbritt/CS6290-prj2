61a9b673dd65adfcc2555153a3d93a51d8a00cb0
Sam Britt <samuelbritt@gmail.com>
2012-04-06 14:37:33 -0400

    udpate validation script

--- tests/validation.py
+++ tests/validation.py
@@ -4,20 +4,39 @@
 #
 # Validation Script
 #
-# To run all the validation traces, and output a diff comparing to the output
-# provided by the TA, run the script with no arguments
+# Runs one or more simulations and can compare with the validation files
+# provided by the TA.
 #
-# To run a specific run, give the name of the run as the first arg, e.g.
+# When run with no arguments, runs all the validation traces and outputs to
+# stdout:
+#
+#       ./validation.py
+#
+# When run with a single validation file input, will only run that simulation:
+#
 #       ./validation.py gcc1.txt
 #
-# To run a specific run, but instead of showing the output, show a diff
-# comparing to the output provided by the TA, run with 'diff' as the second
-# argument, e.g.
-#       ./validation.py gcc1.txt diff
+# Options:
+#   -h, --help
+#           Print a help message and exit.
+#
+#   -d, --diff
+#           Instead of printing the simulation output to stdout, run a diff from
+#           sample output provided by the TA to the simulation output, and print
+#           to stdout.
+#
+#   -q, --quiet
+#           Turn off all verbosity from the simulations (only print statistics
+#           results). If this option is not provided, verbose mode is turned on
+#           in the simulation only if it is turned on for the sample output
+#           provided by the TA. Note that, if both --quiet and --diff are
+#           specified, a large diff could be produced (because the sample files
+#           are verbose but the simulation output is not).
 
 import os
 import sys
 import subprocess
+import argparse
 
 # Usage: tomalulo_sim [-v] <fetch-rate> <k0> <k1> <k2> <cdb-count> <trace_file>
 validation_runs = {
@@ -38,43 +57,68 @@ validation_runs = {
     "barnes-big.txt": {'opts': (4, 3, 3, 3, 4), 'verbose':  True, 'trace': "barnes.txt"},
 }
 
-traces_dir = "traces"
-valid_dir  = "tests/validation"
-prog       = "./tomasulo_sim"
+script_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
+root_dir   = os.path.join(script_dir, "..")
+traces_dir = os.path.join(root_dir, "traces")
+valid_dir  = os.path.join(script_dir, "validation")
+prog       = os.path.join(root_dir, "tomasulo_sim")
 diff       = "diff -u"
 
-def run_cmd(validation_file):
+def run_cmd(validation_file, quiet=False):
     run = validation_runs[validation_file]
     cmd = prog + " " + " ".join(str(opt) for opt in run['opts'])
-    if run['verbose']:
+    if run['verbose'] and not quiet:
         cmd += " -v"
-    cmd += " " + os.path.join(traces_dir, run['trace'])
+    relative_path = os.path.relpath(traces_dir, start=root_dir)
+    cmd += " " + os.path.join(relative_path, run['trace'])
     return cmd
 
-def diff_cmd(validation_file):
-    cmd = run_cmd(validation_file)
+def diff_cmd(validation_file, quiet):
+    cmd = run_cmd(validation_file, quiet)
     cmd += " | {} {} -".format(diff, os.path.join(valid_dir, validation_file))
     return cmd
 
-if __name__ == '__main__':
-    args = sys.argv[1:]
-    if len(args) == 0:
-        # No arguments: run a diff on all the short validation runs
-        short_runs = [r for r in validation_runs if "big" not in r]
-        for r in short_runs:
-            c = diff_cmd(r)
-            subprocess.call(c, shell=True)
-    else:
-        validation_file = args[0]
-        try:
-            r = validation_runs[validation_file]
-        except KeyError:
-            print "Invalid validation file"
-            sys.exit(1)
-
-        if len(args) > 1 and args[1] == "diff":
-            c = diff_cmd(validation_file)
-        else:
-            c = run_cmd(validation_file)
-
+def run_validation_files(cmd_func, files, quiet=False):
+    for r in files:
+        print
+        print r + ":"
+        sys.stdout.flush()
+        c = cmd_func(r, quiet)
         subprocess.call(c, shell=True)
+
+def get_short_runs():
+    return [r for r in validation_runs if "big" not in r]
+
+def valid_run_name_or_exit(filename):
+    validation_file = os.path.basename(filename)
+    if validation_file not in validation_runs:
+        print "Error: invalid validation file"
+        sys.exit(1)
+    return validation_file
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--diff', dest='diff', action='store_true',
+                        help="Show a full diff of simulation output and the "
+                        "provided validation file")
+    parser.add_argument('-q', '--quiet', dest='quiet', action='store_true',
+                        help="Run simulations in quiet (not verbose) mode. "
+                        "Without this option, verbose mode is enabled if the "
+                        "provided validation file was run in verbose mode.")
+    parser.add_argument('validation_file', metavar='<validation_file>', nargs='?',
+                        help="Validation file to compare simulation output to.")
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    cmd_func = run_cmd
+    if args.diff:
+        cmd_func = diff_cmd
+
+    if args.validation_file:
+        run_name = valid_run_name_or_exit(args.validation_file)
+        runs = [run_name]
+    else:
+        runs = sorted(validation_runs.keys())
+    run_validation_files(cmd_func, runs, args.quiet)
